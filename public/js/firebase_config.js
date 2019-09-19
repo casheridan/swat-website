388629d70f83ee6c8b5c0b27fb3f2787627ea724
Sheridan <CS068617@cerner.net>
2019-09-19 09:36:22 -0500

    Added the other pages and got a basic login system with firebase

---
+++ public/js/firebase_config.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCTZd9Va6rTmpnIvrw9RhRHGKl0N5PZ86w",
+    authDomain: "swat-1806-website.firebaseapp.com",
+    databaseURL: "https://swat-1806-website.firebaseio.com",
+    projectId: "swat-1806-website",
+    storageBucket: "",
+    messagingSenderId: "976295716225",
+    appId: "1:976295716225:web:89bc8fd2cbc14a80373480"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
---
+++ public/js/login-system.js
@@ -0,0 +1,32 @@
+// This is used to create a new user with their password and should mostly be used with administrator privilages
+// This could also be used in the future with maybe anyone can sign up and admins can set if you are a student of swat
+document.getElementById("btnSignUp").addEventListener('click', e=>{
+  const email = document.getElementById("email").value;
+  const pass = document.getElementById("password").value;
+  firebase.auth().createUserWithEmailAndPassword(email, pass).catch(function(error) {
+   console.log(error.message);
+  });
+})
+
+// This is the sign in system here, it sends an observer to the console and
+// the console checks the verification of the sent params and decides there
+document.getElementById("btnLogin").addEventListener('click', e=>{
+  const email = document.getElementById("email").value;
+  const pass = document.getElementById("password").value;
+  const promise = firebase.auth().signInWithEmailAndPassword(email, pass);
+  promise.catch(e=>{ console.log(e.massage)})
+})
+
+// Signs the user out
+document.getElementById("btnLogOut").addEventListener('click', e=>{
+  firebase.auth().signOut();
+  console.log('logged out')
+})
+
+firebase.auth().onAuthStateChanged(user=>{
+  if(user){
+    document.getElementById("btnLogOut").classList.remove('hide')
+  } else{
+    document.getElementById("btnLogOut").classList.add('hide')
+  }
+})
