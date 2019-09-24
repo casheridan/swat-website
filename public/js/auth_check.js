536ca046f9e2aa1cc57a14894a26b2df3902c4cd
Christian Sheridan <christian.a.sheridan@cerner.com>
2019-09-24 14:28:23 -0500

    Added Logged in User button and added it to all pages

---
+++ public/js/auth_check.js
@@ -0,0 +1,11 @@
+firebase.auth().onAuthStateChanged(user=>{
+  if(user){
+    document.getElementById("navbar-user").classList.remove('hide')
+    document.getElementById("login_pic-nav").classList.add('hide')
+    document.getElementById("login-nav").classList.add('hide')
+  } else{''
+    document.getElementById("navbar-user").classList.add('hide')
+    document.getElementById("login_pic-nav").classList.remove('hide')
+    document.getElementById("login-nav").classList.remove('hide')
+  }
+})
--- public/js/login-system.js
+++ public/js/login-system.js
@@ -26,7 +26,13 @@ document.getElementById("btnLogOut").addEventListener('click', e=>{
 firebase.auth().onAuthStateChanged(user=>{
   if(user){
     document.getElementById("btnLogOut").classList.remove('hide')
+    document.getElementById("navbar-user").classList.remove('hide')
+    document.getElementById("login_pic-nav").classList.add('hide')
+    document.getElementById("login-nav").classList.add('hide')
   } else{
     document.getElementById("btnLogOut").classList.add('hide')
+    document.getElementById("navbar-user").classList.add('hide')
+    document.getElementById("login_pic-nav").classList.remove('hide')
+    document.getElementById("login-nav").classList.remove('hide')
   }
 })
