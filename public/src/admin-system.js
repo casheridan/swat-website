1179d21773683be0adab3eab3ea33089f576af40
casheridan <prcashnation@gmail.com>
2020-02-15 20:04:04 -0600

    Added blog and blog functionality

--- public/src/admin-system.js
+++ public/src/admin-system.js
@@ -10,6 +10,7 @@ firebase.auth().onAuthStateChanged(user=>{
     if(user){
       // If the user is signed in check permissions
       isAdmin();
+      isWriter(); 
     } else {
       // If not clocked in do nothing
       return;
@@ -18,6 +19,8 @@ firebase.auth().onAuthStateChanged(user=>{
 
 document.getElementById("adminpage").classList.add('hide');
 document.getElementById("adminview").classList.add('hide');
+$('#writerview').hide();
+$('#writerpage').hide();
 
 // Checks if user is an admin if userType is an admin show admin abilities
 // if userType is not a student or admin (0), hide clocking system
@@ -33,5 +36,12 @@ function isAdmin(){
     document.getElementById("time").style.display = "none";
     document.getElementById("clockIn").style.display = "none";
     document.getElementById("clockOut").style.display = "none";
+  }
+}
+
+function isWriter() {
+  if(obj.userType >= 2) {
+    $('#writerview').show();
+    $('#writerpage').show();
   }
 }
