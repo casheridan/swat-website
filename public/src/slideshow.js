aaf5dbb2ecfc1e6a7cc76d6d41f663122a4df7fa
Dillon Woollums <dwooll94@gmail.com>
2023-12-10 15:52:02 -0600

    fix slideshow buttons make pause function

--- public/src/slideshow.js
+++ public/src/slideshow.js
@@ -1,6 +1,8 @@
 var slideIndex = 1;
+var currentTimeoutId = "none";
+var paused = false;
+
 showDivs(slideIndex);
-setTimeout(carousel, 10000);
 
 function plusDivs(n) {
   showDivs(slideIndex += n);
@@ -11,6 +13,7 @@ function currentDiv(n) {
 }
 
 function showDivs(n) {
+  stopCarousel();
   var i;
   var x = document.getElementsByClassName("slides");
   var dots = document.getElementsByClassName("dots");
@@ -24,10 +27,41 @@ function showDivs(n) {
   }
   x[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " w3-red";
+  startCarousel();
 }
 
 function carousel(){
     plusDivs(1);
-    setTimeout(carousel, 10000);
-    
+}
+
+function stopCarousel(){
+  if (typeof this.currentTimeoutId === "number") {
+    clearTimeout(this.currentTimeoutId)
+    this.currentTimeoutId = "none";
+  }
+}
+
+function startCarousel(){
+  currentTimeoutId= setTimeout(carousel, 10000);
+}
+
+function togglePauseCarousel(){
+  if(this.paused){
+    startCarousel();
+    var pauseButtons = document.getElementsByClassName("pausebutton");
+    for (i = 0; i < pauseButtons.length; i++) {
+      pauseButtons[i].innerHTML = "⏸︎";
+    }
+    this.paused = false;
+  }
+  else{
+    stopCarousel();
+    var pauseButtons = document.getElementsByClassName("pausebutton");
+    for (i = 0; i < pauseButtons.length; i++) {
+      
+      pauseButtons[i].innerHTML = "⏵︎";
+    }
+    this.paused = true;
+  }
+
 }
